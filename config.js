6486ba94f3a975c64ff529d2d420246fcd46ddde
benjaminbloch <153328914+benjaminbloch@users.noreply.github.com>
2024-05-19 18:24:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-    "registercommands" : false, //Write True If You Are Launching The Bot First Time
+    "registercommands" : true, //Write True If You Are Launching The Bot First Time
      "token": "", //Your Super Secret Bot Token
     "imageapi": "", //Your Amethyste Api You can get it from https://api.amethyste.moe/
-    "ownerID": [], //Your Discord User ID
+    "ownerID": [369179538615173121, 1240052316342718565], //Your Discord User ID
 "prefix": "", //Your Bot's Prefix
 "chat": {
         "url": "",
@@ -13,8 +13,8 @@ module.exports = {
     }, // You can get This Things from https://brainshop.ai
 "api": "", // Your Youtube Api
 "youtubeAPI": "", // Your Youtube Api
-mainprefix: "",  // Again Your Prefix
-"owner": "G U D B O Y", // Owner Name
+mainprefix: "!",  // Again Your Prefix
+"owner": "R8A1", // Owner Name
 
   basiclang: "en", //The basic language of the bot, "fr" for French and "en" for English
     embeds: {
